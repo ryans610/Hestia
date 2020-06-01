c936e91660c9266b1988774700e706cbd884c6a3
ryans610 <ryans610@gmail.com>
2020-06-01 17:10:34 +0800

    sync

---
+++ RyanJuan.Hestia/Task/FireAndForget.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RyanJuan.Hestia
+{
+    public static partial class HestiaTask
+    {
+#if !NET40
+#if ZH_HANT
+#else
+#endif
+        public static async void FireAndForget(
+            this Task task,
+            Action<Exception>? exceptionHandler = null)
+        {
+            Error.ThrowIfArgumentNull(nameof(task), task);
+            try
+            {
+                await task;
+            }
+            catch (Exception ex)
+            {
+                if (exceptionHandler is { })
+                {
+                    exceptionHandler(ex);
+                }
+            }
+        }
+#endif
+    }
+}
